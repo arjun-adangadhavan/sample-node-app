8124f69736cf699fca647cd710a144f78295584e
arjun-adangadhavan <147017045+arjun-adangadhavan@users.noreply.github.com>
2023-11-23 19:12:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 9000;
 app.get('/', (req, res) => {
-    res.send('<h1>FREE-STYLE-NODEJS-PROJECT!</h1>');
+    res.send('<h1>FREE-STYLE-NODEJS-SCRIPTED-PROJECT!</h1>');
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
